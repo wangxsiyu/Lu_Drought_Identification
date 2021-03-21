96c8ca11860e663e2a30b96afa68b6da87588b8b
wangxsiyu <wangxsiyu@gmail.com>
2021-03-21 12:25:39 -0400

    mdzz

--- code_data/func_process.R
+++ code_data/func_process.R
@@ -1,6 +1,25 @@
+preprocess_average <- function(d, nav = 1){
+  ymd = c('year','month','day')
+  sf = d[, setdiff(names(d),ymd)]
+  sf_afma = sf
+  for(sfi in 1:ncol(sf)){
+    Q_ma = mam(sf[,sfi], nav)
+    sf_afma[,sfi] = Q_ma
+  }
+  sf_ma = cbind(d[,ymd],sf_afma)
+  return(sf_ma)
+}
+
+mam <- function(x,nav){
+  for (i in length(x):(nav+1)){
+     x[i] = mean(x[(i-nav):i], na.rm = T)
+  }
+  return(x)
+}
+
 ma <- function(x,M=30){filter(x,rep(1/M,M),sides=1)}
 preprocess <- function(d, area = NULL){
-  doy = get_dayid(d$month, d$day)
+  # doy = get_dayid(d$month, d$day)
   ymd = c('year','month','day')
   sf = d[, setdiff(names(d),ymd)]
   sf_afma = sf
@@ -16,13 +35,13 @@ preprocess <- function(d, area = NULL){
   sf_ma = cbind(d[,ymd],sf_afma)
   return(sf_ma)
 }
-get_dayid <- function(M,D, option29 = 0){
-  y = matrix(NaN, length(D),1)
-  ms = c(31,28 + option29,31,30,31,30,31,31,30,31,30)
-  ms = c(0, cumsum(ms))
-  for (i in 1:length(D)){
-    y[i] = ms[M[i]] + D[i]
-  }
-  y = y[,1]
-  return(y)
-}
+# get_dayid <- function(M,D, option29 = 0){
+#   y = matrix(NaN, length(D),1)
+#   ms = c(31,28 + option29,31,30,31,30,31,31,30,31,30)
+#   ms = c(0, cumsum(ms))
+#   for (i in 1:length(D)){
+#     y[i] = ms[M[i]] + D[i]
+#   }
+#   y = y[,1]
+#   return(y)
+# }
--- code_data/main_smooth.R
+++ code_data/main_smooth.R
@@ -4,5 +4,16 @@ setwd(funcdir)
 datadir = "../../data"
 source('func_process.R')
 data = read.csv(file.path(datadir, 'runoff','runoff_raw.csv'))
-data = preprocess(data)
-write.csv(data,file.path(datadir, 'runoff', 'runoff.csv'))
+tdata = preprocess(data)
+write.csv(tdata,file.path(datadir, 'runoff', 'runoff.csv'))
+
+
+############################################
+library(stringr)
+namepc = 'perc_T0_C0_fr0_1912_2020.csv'
+perc = read.csv(file.path(datadir, 'perc',namepc))
+for (i in 1:7){
+  tperc = preprocess_average(perc, i)
+  tstr = substr(namepc,1, str_length(namepc)-4)
+  write.csv(tperc,file.path(datadir, 'perc', tstr, sprintf('%s_av%d.csv',tstr, i)))
+}
